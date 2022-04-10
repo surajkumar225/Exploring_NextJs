6df3f41ae7f324cd208906f0b56e1daa7eb076dc
Suraj kumar <96677059+surajkumar225@users.noreply.github.com>
2022-04-10 13:22:14 +0530

    NextJs

---
+++ dawson/pages/about.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>Hey Trase! This is my about page</div>
+  )
+}
+
+export default about
---
+++ dawson/pages/blogpost/travel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const travel = () => {
+  return (
+    <div>
+      <h1>Travelling all around the world..</h1>
+    </div>
+  )
+}
+
+export default travel
